456e22123db5da331291a01914c3cae303101157
Piotr Bednarek <piotrbed5@wp.pl>
2015-01-24 11:11:46 +0100

    tekst literka po literce

--- Assets/ConsoleScript.cs
+++ Assets/ConsoleScript.cs
@@ -3,23 +3,28 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ConsoleScript : MonoBehaviour {
-	string sytuacja = "Pierwszy tekst wyświetlany za pomoca textfielda, powinien wyświetlać się literka po literce";
+	string sytuacja = "Pierwszy tekst wyświetlany za pomoca textfielda, powinien wyświetlać się literka po literce.";
 	string tmp="";
 	Text text;
+	int obliczenia;
+	int size;
+	bool isAllText;
 	// Use this for initialization
 	void Start () {
 		text = GameObject.Find("Konsola").GetComponent<Text>();
-		for (int i=0; i<sytuacja.Length; i++) {
-
-			tmp+=sytuacja[i];
-			text.text=tmp;
-			System.Threading.Thread.Sleep(2);
-				}
-
+		obliczenia = 0;
+		size = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (size < sytuacja.Length) {
+			if((obliczenia++)%3==0){
+				tmp+=sytuacja[size];
+				text.text=tmp;
+				size++;
+			}
+
+		}
 	}
 }
