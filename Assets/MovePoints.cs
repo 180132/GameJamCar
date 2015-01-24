45e98d678789f400c8f3899aefbbe805f745cfdc
Adam <1993bobek1@gmail.com>
2015-01-24 21:48:05 +0100

    movement of car - buttons

--- Assets/MovePoints.cs
+++ Assets/MovePoints.cs
@@ -9,6 +9,17 @@ public class MovePoints : MonoBehaviour {
 	public GameObject waypointB;
 	public GameObject waypointC;
 	public GameObject waypointD;
+	public GameObject waypointE;
+	public GameObject waypointF;
+	public GameObject waypointG;
+	public GameObject waypointH;
+	public GameObject waypointI;
+	public GameObject waypointJ;
+	public GameObject waypointK;
+	public GameObject waypointL;
+	public GameObject waypointM;
+
+
 	public float speed = 1;
 	private bool directionAB = true;
 	private GameObject destination;
@@ -27,17 +38,17 @@ public class MovePoints : MonoBehaviour {
 		{
 			if(p == destination.transform.position)
 			{
-
+				ruch = false;
 				changeDir();
 				if(collision!= null){
-					Debug.Log (""); 
 					collision();
+
 				}
 			}
 		}
 		if (distanceCheck() == false)
 			Debug.Log("w zaSIEGU");
-
+		if(ruch)
 			this.transform.position = Vector3.MoveTowards (this.transform.position, 
 			                                              destination.transform.position,
 		                                              speed * Time.fixedDeltaTime);
@@ -45,15 +56,63 @@ public class MovePoints : MonoBehaviour {
 
 
 	void changeDir() {
-		if(destination.transform.position == waypointA.transform.position)
-			destination = waypointB;
-		else
-			if (destination.transform.position == waypointB.transform.position)
-				destination = waypointC;
-			else
-				if (destination.transform.position == waypointC.transform.position)
-					destination = waypointD;
-	}
+		if (destination.transform.position == waypointA.transform.position) {
+			ConsoleScript.scen = 0;
+						destination = waypointB;
+				} else
+			if (destination.transform.position == waypointB.transform.position) {
+			ConsoleScript.scen = 1;
+						destination = waypointC;
+				} else
+				if (destination.transform.position == waypointC.transform.position) {
+			ConsoleScript.scen = 2;
+						destination = waypointD;
+				} else
+					if (destination.transform.position == waypointD.transform.position) {
+			ConsoleScript.scen = 3;
+						destination = waypointE;
+				} else
+						if (destination.transform.position == waypointE.transform.position) {
+			ConsoleScript.scen = 4;
+						destination = waypointF;
+				} else
+							if (destination.transform.position == waypointF.transform.position) {
+			ConsoleScript.scen = 5;
+						destination = waypointG;
+				} else
+								if (destination.transform.position == waypointG.transform.position) {
+			ConsoleScript.scen = 6;
+						destination = waypointH;
+				} else
+									if (destination.transform.position == waypointH.transform.position) {
+			ConsoleScript.scen = 7;
+						destination = waypointI;
+				} else
+										if (destination.transform.position == waypointI.transform.position) {
+			ConsoleScript.scen = 8;
+						destination = waypointJ;
+				} else 
+											if (destination.transform.position == waypointI.transform.position) {
+			ConsoleScript.scen = 9;
+						destination = waypointJ;
+				} else
+												if (destination.transform.position == waypointJ.transform.position) {
+			ConsoleScript.scen = 10;
+						destination = waypointK;
+				} else
+													if (destination.transform.position == waypointK.transform.position) {
+			ConsoleScript.scen = 11;
+						destination = waypointL;
+				} else
+														if (destination.transform.position == waypointL.transform.position) {
+			ConsoleScript.scen = 12;
+						destination = waypointM;
+				}
+															if (destination.transform.position == waypointL.transform.position) {
+			ConsoleScript.scen = 13;
+				}
+											
+	}	
 
 
 	bool distanceCheck() {
